cdef6353ea72d6bbbd8d28acbe4a35cb79036f49
Bread God <Vipcube@users.noreply.github.com>
2022-03-26 16:34:44 +0800

    Add the grpc client module.

--- gRPC-Server/src/main/java/org/vipcube/spring/grpc/server/SimpleServerApplication.java
+++ gRPC-Server/src/main/java/org/vipcube/spring/grpc/server/SimpleServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class SimpleServerApplication {
 	public static void main( String[] args ){
-		SpringApplication.run( ServerApplication.class, args );
+		SpringApplication.run( SimpleServerApplication.class, args );
 	}
 }
